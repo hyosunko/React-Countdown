d10055d03e695f6cd37a0164fa4e61f77ea977fd
hyosunko <hyosunko@gmail.com>
2019-05-06 18:34:26 -0700

    Updates

---
+++ precache-manifest.18f4818c858c7889b2076a0e7e5d613e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6487ee1f9a5d0ddb539b1eca0b74f0d",
+    "url": "/React-Countdown/index.html"
+  },
+  {
+    "revision": "7207e30441c0c73874d8",
+    "url": "/React-Countdown/static/css/2.4223df75.chunk.css"
+  },
+  {
+    "revision": "7207e30441c0c73874d8",
+    "url": "/React-Countdown/static/js/2.2ceaa05c.chunk.js"
+  },
+  {
+    "revision": "15946202ce1d1cb24038",
+    "url": "/React-Countdown/static/js/main.7a9c5eb4.chunk.js"
+  },
+  {
+    "revision": "787b38b62d10eab1becf",
+    "url": "/React-Countdown/static/js/runtime~main.2cd0d61b.js"
+  }
+]);
--- src/components/App.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import Countdown from "./Countdown/";
-
-export default props => <Countdown />;
